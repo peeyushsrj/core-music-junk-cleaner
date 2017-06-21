a9ef29c8e9991ded67fb25b7d3e7a6a5066747fd
hsuyeep <peeyushsrj@gmail.com>
2017-06-21 19:03:45 +0530

    code refactor 1

--- new_spam.go
+++ new_spam.go
@@ -11,15 +11,48 @@ import (
 	"path/filepath"
 	"regexp"
 	"strings"
-	_ "time"
 )
 
 var (
 	musicList []string
 	spamList  []string //Loaded from database
 	upgrader  = websocket.Upgrader{}
-	new_fi    string
 )
+
+func LinesFromFile(path string) ([]string, error) {
+	var arr []string
+
+	file, err := os.OpenFile(path, os.O_RDONLY|os.O_CREATE, 0666)
+	if err != nil {
+		return arr, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		arr = append(arr, strings.TrimSpace(scanner.Text()))
+	}
+	if scanner.Err() != nil {
+		return arr, scanner.Err()
+	}
+	return arr, nil
+}
+
+func BrowseXFiles(x string, root string) ([]string, error) {
+	var arr []string
+	err := filepath.Walk(root, func(path string, f os.FileInfo, err error) error {
+		if !f.IsDir() {
+			if strings.HasSuffix(f.Name(), x) { //.mp3
+				arr = append(arr, path)
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return arr, err
+	}
+	return arr, nil
+}
 
 func main() {
 	if len(os.Args) == 1 {
@@ -27,39 +60,20 @@ func main() {
 		return
 	}
 
-	//load spam list
-	file, err := os.OpenFile("spam.txt", os.O_RDONLY|os.O_CREATE, 0666)
+	//load local spam list
+	spamList, err := main.LinesFromFile("./spam.txt")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error loading in spamlist", err)
 	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		spamList = append(spamList, strings.TrimSpace(scanner.Text()))
-	}
-	if scanner.Err() != nil {
-		fmt.Println(scanner.Err())
-	}
-	fmt.Println(spamList)
 
 	// musicList = []string{}
-	err = filepath.Walk(os.Args[1:][0], func(path string, f os.FileInfo, err error) error {
-		if !f.IsDir() {
-			if strings.HasSuffix(f.Name(), ".mp3") {
-				musicList = append(musicList, path)
-			}
-		}
-		return nil
-	})
+	musicList, err = main.BrowseXFiles(".mp2", os.Args[1:][0])
 	if err != nil {
-		log.Fatal("Error in walking: ", err)
+		log.Fatal("Error in walking over files", err)
 	}
 
-	http.HandleFunc("/", Hhandler)
+	http.HandleFunc("/", home)
 	http.HandleFunc("/ws", compute)
-	http.HandleFunc("/bs", bs)
-
 	log.Println("Running on :7899")
 	err = http.ListenAndServe(":7899", nil)
 	if err != nil {
@@ -67,11 +81,8 @@ func main() {
 	}
 }
 
-func bs(rw http.ResponseWriter, req *http.Request) {
-	http.ServeFile(rw, req, "bootstrap.min.css")
-}
-func Hhandler(rw http.ResponseWriter, req *http.Request) {
-	fmt.Println("Client connected", req.RemoteAddr)
+func home(rw http.ResponseWriter, req *http.Request) {
+	// fmt.Println("Client connected", req.RemoteAddr)
 	var v = struct {
 		Host  string
 		Count int
@@ -83,6 +94,11 @@ func Hhandler(rw http.ResponseWriter, req *http.Request) {
 	t.Execute(rw, &v)
 }
 
+type ResponseMsg struct {
+	Count   int
+	Context string
+}
+
 func compute(w http.ResponseWriter, r *http.Request) {
 	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
@@ -91,56 +107,41 @@ func compute(w http.ResponseWriter, r *http.Request) {
 	}
 	defer c.Close()
 
+	var cleaned_fi string
+	var spam string
+	var iter = 0
+
+	//Regex for websites spam
 	webRex := "(www.|)[a-zA-Z0-9_\\-]+\\.[a-zA-Z]{2,4}"
 	rx, _ := regexp.Compile(webRex)
-	var fi_mod string
-	iter := 0
+
+	//Scan music list
 	for _, fi := range musicList {
 		iter = iter + 1
-		//Cut mp3 suffix & we need to check for mp3 file only not folder
-		fi_mod = filepath.Base(strings.TrimSuffix(fi, ".mp3"))
-		//Only Website named songs - maybe spam websites
-		if rx.MatchString(fi_mod) {
+
+		//Only base names, mp3 extension exclude
+		cleaned_fi = filepath.Base(strings.TrimSuffix(fi, ".mp3"))
+
+		//Possible spam with websites name, other exclude
+		if rx.MatchString(cleaned_fi) {
+
 			//Spam List Match
-			var flag = false
-			var spam = ""
-			for _, sp := range spamList {
-				if strings.Contains(fi_mod, sp) {
-					spam = sp
-					flag = true
-				}
-			}
-			//User need to input spam
-			if spam == "" {
-				var u = struct {
-					Count   int
-					Context string
-				}{
-					iter,
-					fi_mod,
-				}
-				c.WriteJSON(&u)
+			spam = str_in_slice(cleaned_fi, spamList)
+			if spam == "" { //spam not found
+				c.WriteJSON(&ResponseMsg{iter, cleaned_fi})
+
 				var v = struct{ Spam string }{}
 				c.ReadJSON(&v)
 				spam = v.Spam
-			}
-			// fmt.Println("SPAM", spam)
-			//Here we'll have spam variable
-			if flag == false {
+
+				//New spam from user added to local spam list
 				spamList = append(spamList, spam)
 				appendToSpamDB(spam)
 			}
-			os.Rename(fi, strings.Replace(fi, spam, "", 1))
+			// os.Rename(fi, strings.Replace(fi, spam, "", 1))
 		}
 	}
-	var u = struct {
-		Count   int
-		Context string
-	}{
-		iter,
-		fi_mod,
-	}
-	c.WriteJSON(&u)
+	c.WriteJSON(&ResponseMsg{iter, cleaned_fi})
 	c.Close()
 }
 
@@ -148,4 +149,13 @@ func appendToSpamDB(sp string) {
 	file, _ := os.OpenFile("spam.txt", os.O_RDWR|os.O_APPEND, 0666)
 	defer file.Close()
 	file.WriteString(sp + "\n")
+}
+
+func str_in_slice(a string, b []string) string {
+	for _, el := range b {
+		if strings.Contains(a, el) {
+			return el
+		}
+	}
+	return ""
 }
